8d274df84a16c744258713e40d9eabc898206ab4
Jiggy079 <jbvickers01@gmail.com>
2022-12-01 18:22:35 +0000

    Add RadioGroupQuestion.js

---
+++ src/RadioGroupQuestion.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+function getQuestionsContent(questions) {
+    let questionsList = [];
+    for (let [value, label] of Object.entries(questions)) {
+        questionsList.push(<FormControlLabel value={value} control={<Radio />} label={label} />);
+    }
+    return questionsList;
+}
+
+function RowRadioGroupQuestion([title, formName, questions]) {
+    
+    return (
+        <FormControl>
+          <FormLabel>{title}</FormLabel>
+          <RadioGroup row name={formName}>
+            {getQuestionsContent(questions)}
+          </RadioGroup>
+        </FormControl>
+      );
+}
+
+export default RowRadioGroupQuestion;
