9e23b3e691c2602e42b473fad844003aaca34baa
Jiggy079 <jbvickers01@gmail.com>
2022-12-01 17:00:27 +0000

    Add font imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 import Figure from "./Figure";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 class App extends React.Component {
     constructor(props) {
