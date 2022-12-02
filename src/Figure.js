d6cc429c8f9a31f2cd497c5d4c55f059f5730a5f
Jiggy079 <jbvickers01@gmail.com>
2022-12-02 16:48:24 +0000

    Fix props not being referenced correctly

--- src/Figure.js
+++ src/Figure.js
@@ -6,7 +6,9 @@ class Figure extends React.Component{
 	constructor({imgUrl, currentFigureIndex}) {
 		super(imgUrl, currentFigureIndex);
 		 this.state = {
-			 open: false
+			 open: false,
+			 imgUrl: imgUrl,
+			 currentFigureIndex: currentFigureIndex
 		 };
 	}
 
@@ -22,12 +24,12 @@ class Figure extends React.Component{
 		return (
 			<div className="Figure">
 				<Button onClick={this.handleToggle}>
-				<img src={imgUrl} alt={"Figure"}/>
+				<img src={this.state.imgUrl} alt={"Figure"}/>
 				</Button>
 				<Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={this.state.open} onClick={this.handleClose}>
 					<h1>hello</h1>
 				</Backdrop>
-				<p id={"figure-label"}>Figure {currentFigureIndex + 1}</p>
+				<p id={"figure-label"}>Figure {this.state.currentFigureIndex + 1}</p>
 			</div>
 		);
 	}
