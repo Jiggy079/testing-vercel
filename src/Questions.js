b388e58362016c6606624eabb064554f38770ea2
Jiggy079 <jbvickers01@gmail.com>
2022-12-02 18:20:06 +0000

    Test Cards instead of Paper

--- src/Questions.js
+++ src/Questions.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 import RowRadioGroupQuestion from "./RadioGroupQuestion";
 
 function Questions() {
@@ -11,14 +14,14 @@ function Questions() {
 	return (
 		<Grid sx={{flexGrow: 1}} container spacing={2}>
 			<Grid item xs={6}>
-				<Paper sx={{backgroundColor: '#1A2027', color:'#61dafb'}}>
+				<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250}}>
 					<RowRadioGroupQuestion title="Test question" formName="test" questions={testQuestion} />
-				</Paper>
+				</Card>
 			</Grid>
 			<Grid item xs={6}>
-				<Paper sx={{backgroundColor: '#1A2027', color:'#61dafb'}}>
-					<p>test</p>
-				</Paper>
+				<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250}}>
+					<RowRadioGroupQuestion title="Test question" formName="test" questions={testQuestion} />
+				</Card>
 			</Grid>
 			<Grid item xs={6}>
 				<Paper sx={{backgroundColor: '#1A2027', color:'#61dafb'}}>
