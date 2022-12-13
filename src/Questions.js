bd578e6e9bd572430e453ec4d9415c06416be064
Jiggy079 <jbvickers01@gmail.com>
2022-12-13 18:19:16 +0000

    Change Questions.js to class component since state will be needed Refactor radio questions and questions objects

--- src/Questions.js
+++ src/Questions.js
@@ -7,85 +7,106 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
-function Questions() {
-	let question1 = {
-		"blackwhite": "Black and white",
-		"greyscale": "Greyscale",
-		"colour": "Colour"
-	}
-	let question2 = {
-		"aesthetics": "Aesthetics",
-		"datavis": "Data visualisation",
-		"notsure": "Not sure"
-	}
-	let question3 = {
-		"yes": "Yes",
-		"no": "No",
-		"notsure": "Not sure"
-	}
-	let question4 = {
-		"continuous": "Continuous",
-		"categorical": "Categorical",
-		"notsure": "Not sure"
-	}
-	let question6 = {
-		"1": "1",
-		"2": "2",
-		"3": "3",
-		"4": "4",
-		"5": "5"
+class Questions extends React.Component {
+	constructor() {
+		super();
+		this.getQuestions = this.getQuestions.bind(this);
 	}
 
-	return (
-		<Stack direction="column" justifyContent="flex-start" alignItems="center" spacing={2}>
-			<Grid sx={{flexGrow: 1}} container spacing={2}>
-				<Grid item xs={6}>
-					<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250, minHeight: 175}}>
-						<CardContent>
-							<RowRadioGroupQuestion title="Q1. What type of colour is used?" formName="question1" question={question1} />
-						</CardContent>
-					</Card>
+	getQuestions() {
+		let question1 = {
+			"blackwhite": "Black and white",
+			"greyscale": "Greyscale",
+			"colour": "Colour"
+		}
+		let question2 = {
+			"aesthetics": "Aesthetics",
+			"datavis": "Data visualisation",
+			"notsure": "Not sure"
+		}
+		let question3 = {
+			"yes": "Yes",
+			"no": "No",
+			"notsure": "Not sure"
+		}
+		let question4 = {
+			"continuous": "Continuous",
+			"categorical": "Categorical",
+			"notsure": "Not sure"
+		}
+		let question6 = {
+			"1": "1",
+			"2": "2",
+			"3": "3",
+			"4": "4",
+			"5": "5"
+		}
+		return [question1, question2, question3, question4, question6];
+	}
+
+	render() {
+		let questions = this.getQuestions();
+		return (
+			<Stack direction="column" justifyContent="flex-start" alignItems="center" spacing={2}>
+				<Grid sx={{flexGrow: 1}} container spacing={2}>
+					<Grid item xs={6}>
+						<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250, minHeight: 175}}>
+							<CardContent>
+								<RowRadioGroupQuestion
+									title="Q1. What type of colour is used?"
+									formName="question1" question={questions[0]} />
+							</CardContent>
+						</Card>
+					</Grid>
+					<Grid item xs={6}>
+						<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250, minHeight: 175}}>
+							<CardContent>
+								<RowRadioGroupQuestion
+									title="Q2. Is colour used for aesthetics or data visualisation?"
+									formName="question2" question={questions[1]} />
+							</CardContent>
+						</Card>
+					</Grid>
+					<Grid item xs={6}>
+						<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250, minHeight: 175}}>
+							<CardContent>
+								<RowRadioGroupQuestion
+									title="Q3. Is a colour mapping legend shown?"
+									formName="question3" question={questions[2]} />
+							</CardContent>
+						</Card>
+					</Grid>
+					<Grid item xs={6}>
+						<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250, minHeight: 175}}>
+							<CardContent>
+								<RowRadioGroupQuestion
+									title="Q4. Is the colour mapping continuous or categorical?"
+									formName="question4" question={questions[3]} />
+							</CardContent>
+						</Card>
+					</Grid>
+					<Grid item xs={6}>
+						<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250, minHeight: 175}}>
+							<CardContent>
+								<p className="questionLabel">Q5. How many colour values are used?</p>
+								<TextField id="q5-input" label="Enter a number" type="number" name="question5" InputLabelProps={{shrink: true,}} />
+							</CardContent>
+						</Card>
+					</Grid>
+					<Grid item xs={6}>
+						<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250, minHeight: 175}}>
+							<CardContent>
+								<RowRadioGroupQuestion
+									title="Q6. How difficult was this image to annotate? (1 being easy and 5 being hard)"
+									formName="question6" question={questions[4]} />
+							</CardContent>
+						</Card>
+					</Grid>
 				</Grid>
-				<Grid item xs={6}>
-					<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250, minHeight: 175}}>
-						<CardContent>
-							<RowRadioGroupQuestion title="Q2. Is colour used for aesthetics or data visualisation?" formName="question2" question={question2} />
-						</CardContent>
-					</Card>
-				</Grid>
-				<Grid item xs={6}>
-					<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250, minHeight: 175}}>
-						<CardContent>
-							<RowRadioGroupQuestion title="Q3. Is a colour mapping legend shown?" formName="question3" question={question3} />
-						</CardContent>
-					</Card>
-				</Grid>
-				<Grid item xs={6}>
-					<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250, minHeight: 175}}>
-						<CardContent>
-							<RowRadioGroupQuestion title="Q4. Is the colour mapping continuous or categorical?" formName="question4" question={question4} />
-						</CardContent>
-					</Card>
-				</Grid>
-				<Grid item xs={6}>
-					<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250, minHeight: 175}}>
-						<CardContent>
-							<p className="questionLabel">Q5. How many colour values are used?</p>
-							<TextField id="outlined-number" label="Enter a number" type="number" name="question5" InputLabelProps={{shrink: true,}} />
-						</CardContent>
-					</Card>
-				</Grid>
-				<Grid item xs={6}>
-					<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250, minHeight: 175}}>
-						<CardContent>
-							<RowRadioGroupQuestion title="Q6. How difficult was this image to annotate? (1 being easy and 5 being hard)" formName="question6" question={question6} />
-						</CardContent>
-					</Card>
-				</Grid>
-			</Grid>
-			<Button variant="contained">Submit</Button>
-		</Stack>
-	);
+				<Button variant="contained" onClick={this.doSubmit}>Submit</Button>
+			</Stack>
+		);
+	}
 }
 
 export default Questions;
