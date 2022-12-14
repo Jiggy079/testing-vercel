6c4e09c2bccd3dd5731fe00941d0615ead491193
Jiggy079 <jbvickers01@gmail.com>
2022-12-14 15:08:10 +0000

    Test getting value of question answers in doSubmit

--- src/Questions.js
+++ src/Questions.js
@@ -46,7 +46,10 @@ class Questions extends React.Component {
 	}
 
 	doSubmit() {
-		console.log("a");
+		for (let i=0; i < 7; i++) {
+			let q = document.getElementById("question"+i);
+			console.log(q.value);
+		}
 	}
 
 	render() {
@@ -59,7 +62,7 @@ class Questions extends React.Component {
 							<CardContent>
 								<RowRadioGroupQuestion
 									title="Q1. What type of colour is used?"
-									formName="question1" question={questions[0]} />
+									formName="question1" question={questions[0]} index={1} />
 							</CardContent>
 						</Card>
 					</Grid>
@@ -68,7 +71,7 @@ class Questions extends React.Component {
 							<CardContent>
 								<RowRadioGroupQuestion
 									title="Q2. Is colour used for aesthetics or data visualisation?"
-									formName="question2" question={questions[1]} />
+									formName="question2" question={questions[1]} index={2} />
 							</CardContent>
 						</Card>
 					</Grid>
@@ -77,7 +80,7 @@ class Questions extends React.Component {
 							<CardContent>
 								<RowRadioGroupQuestion
 									title="Q3. Is a colour mapping legend shown?"
-									formName="question3" question={questions[2]} />
+									formName="question3" question={questions[2]} index={3} />
 							</CardContent>
 						</Card>
 					</Grid>
@@ -86,7 +89,7 @@ class Questions extends React.Component {
 							<CardContent>
 								<RowRadioGroupQuestion
 									title="Q4. Is the colour mapping continuous or categorical?"
-									formName="question4" question={questions[3]} />
+									formName="question4" question={questions[3]} index={4} />
 							</CardContent>
 						</Card>
 					</Grid>
@@ -94,7 +97,7 @@ class Questions extends React.Component {
 						<Card sx={{backgroundColor: '#1A2027', color:'#61dafb', minWidth: 250, minHeight: 175}}>
 							<CardContent>
 								<p className="questionLabel">Q5. How many colour values are used?</p>
-								<TextField id="q5-input" label="Enter a number" type="number" name="question5" InputLabelProps={{shrink: true,}} />
+								<TextField id="question5" label="Enter a number" type="number" name="question5" InputLabelProps={{shrink: true,}} />
 							</CardContent>
 						</Card>
 					</Grid>
@@ -103,7 +106,7 @@ class Questions extends React.Component {
 							<CardContent>
 								<RowRadioGroupQuestion
 									title="Q6. How difficult was this image to annotate? (1 being easy and 5 being hard)"
-									formName="question6" question={questions[4]} />
+									formName="question6" question={questions[4]} index={6} />
 							</CardContent>
 						</Card>
 					</Grid>
--- src/RadioGroupQuestion.js
+++ src/RadioGroupQuestion.js
@@ -13,12 +13,12 @@ function getQuestionsContent(question) {
     return questionsList;
 }
 
-function RowRadioGroupQuestion({title, formName, question}) {
+function RowRadioGroupQuestion({title, formName, question, index}) {
     
     return (
         <FormControl>
           <FormLabel><span className="questionLabel"> {title}</span></FormLabel>
-          <RadioGroup row name={formName}>
+          <RadioGroup row name={formName} id={"question"+index}>
             {getQuestionsContent(question)}
           </RadioGroup>
         </FormControl>
