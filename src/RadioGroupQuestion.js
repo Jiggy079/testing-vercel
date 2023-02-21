6b4a447b3f7f1b57b6bd278678c67f71c694bf1a
Jiggy079 <jbvickers01@gmail.com>
2023-02-21 03:03:54 +0000

    Change RadioGroupQuestion from functional component to class component to see if it helps at all with defaultValue issues

--- src/RadioGroupQuestion.js
+++ src/RadioGroupQuestion.js
@@ -5,35 +5,53 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 
-function getQuestionsContent(question) {
-    let questionsList = [];
-    for (let [value, label] of Object.entries(question)) {
-        questionsList.push(<FormControlLabel value={value} control={<Radio />} label={label} />);
-    }
-    return questionsList;
-}
+class RowRadioGroupQuestion extends React.Component {
+    title = null;
+    formName = null;
+    question = null;
+    index = null;
+    defaultValue = null;
 
-function RowRadioGroupQuestion({title, formName, question, index, defValue}) {
-    if (defValue == null) {
-        console.log("rendered radio with defaultValue=null");
-        return (
-            <FormControl>
-                <FormLabel><span className="questionLabel"> {title}</span></FormLabel>
-                <RadioGroup row name={formName} id={"question"+index}>
-                    {getQuestionsContent(question)}
-                </RadioGroup>
-            </FormControl>
-        );
-    } else {
-        console.log("rendered radio with defaultValue="+defValue);
-        return (
-            <FormControl>
-                <FormLabel><span className="questionLabel"> {title}</span></FormLabel>
-                <RadioGroup row name={formName} id={"question"+index} defaultValue={defValue.toString()}>
-                    {getQuestionsContent(question)}
-                </RadioGroup>
-            </FormControl>
-        );
+    constructor({title, formName, question, index, defaultValue}) {
+        super({title, formName, question, index, defaultValue});
+        this.title = title;
+        this.formName = formName;
+        this.question = question;
+        this.index = index;
+        this.defaultValue = defaultValue;
+        this.getQuestionsContent = this.getQuestionsContent.bind(this);
+    }
+
+    getQuestionsContent(question) {
+        let questionsList = [];
+        for (let [value, label] of Object.entries(question)) {
+            questionsList.push(<FormControlLabel value={value} control={<Radio />} label={label} />);
+        }
+        return questionsList;
+    }
+
+    render () {
+        if (this.defaultValue == null) {
+            console.log("rendered radio with defaultValue=null");
+            return (
+                <FormControl>
+                    <FormLabel><span className="questionLabel"> {this.title}</span></FormLabel>
+                    <RadioGroup row name={this.formName} id={"question"+this.index}>
+                        {this.getQuestionsContent(this.question)}
+                    </RadioGroup>
+                </FormControl>
+            );
+        } else {
+            console.log("rendered radio with defaultValue="+this.defaultValue);
+            return (
+                <FormControl>
+                    <FormLabel><span className="questionLabel"> {this.title}</span></FormLabel>
+                    <RadioGroup row name={this.formName} id={"question"+this.index} defaultValue={this.defaultValue.toString()}>
+                        {this.getQuestionsContent(this.question)}
+                    </RadioGroup>
+                </FormControl>
+            );
+        }
     }
 }
 
