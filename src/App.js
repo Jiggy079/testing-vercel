1fb72cd3ffc79309101f9479dc589a8494251288
Jiggy079 <jbvickers01@gmail.com>
2023-02-22 04:14:17 +0000

    Change user dropdown styling

--- src/App.js
+++ src/App.js
@@ -113,7 +113,7 @@ class App extends React.Component {
                             <Select
                                 labelStyle = {{color: "#61dafb"}}
                                 value={this.state.user}
-                                label={<span style="color: blue;">User</span>}
+                                label="User"
                                 onChange={this.handleUserChange}
                             >
                                 <MenuItem value={"HL"}>HL</MenuItem>
